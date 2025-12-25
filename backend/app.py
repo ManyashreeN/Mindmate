a3bd9942f4b07188e40fb3d0e27abddccd83bdac
ManyashreeN <145050937+ManyashreeN@users.noreply.github.com>
2025-12-25 07:32:24 +0000

    Third draft of app.py of backend

--- backend/app.py
+++ backend/app.py
@@ -200,4 +200,5 @@ def server_error(error):
 
 if __name__ == "__main__":
     # Development server
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
